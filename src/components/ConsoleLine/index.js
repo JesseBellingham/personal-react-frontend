348835d8a9c4fecfc04194acdce997b19b9abb61
Jesse Bellingham <jesse.bellingham@xero.com>
2020-06-08 20:13:35 +1000

    Console component

---
+++ src/components/ConsoleLine/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+class ConsoleLine extends Component {
+  static displayName = ConsoleLine.name
+
+  constructor(props) {
+    super(props)
+    this.state = { lineTitle: "", lineDescription: "", lineLink: "" }
+  }
+
+  render() {
+    const { lineTitle, lineDescription, lineLink } = this.props
+    return (
+      <div style={{ margin: "1em 2em" }}>
+        <p className="row code-text">>&nbsp;{lineTitle}</p>
+        {(lineLink && <span>=> <a href={lineLink}>{lineDescription}</a></span>) ||
+          (lineDescription && (
+            <p className="row code-text" style={{ paddingLeft: "1em" }}>
+              => {lineDescription}
+            </p>
+          ))}
+      </div>
+    )
+  }
+}
+
+export default ConsoleLine
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,35 @@ import ExperienceSection from '../components/ExperienceSection'
 import ProjectsSection from '../components/ProjectsSection'
 import About from '../components/Sidebar/About'
 import Links from '../components/Sidebar/Links'
+import Console from '../components/Console'
 
 import './style.scss'
 
 const IndexPage = () => (
     <div className="index">
       <div className="main">
-        <h5>
+        
+        <div className="about-me">
+            <div className="panel-inner" style={{ padding: "8em 1em" }}>
+                <p style={{ textAlign: "center", fontSize:"20px" }}>
+                    Hello World!
+                </p>
+                <h1 className="name-header" >
+                    My name is Jesse
+                </h1>
+                <h2 style={{ textAlign: "center", color: "grey", marginBottom: "3em" }} >
+                    I build web applications.
+                </h2>
+                <Console />
+            </div>
+        </div>
+        {/* <h5>
           Hi, I'm <span className="bold">Jesse Bellingham</span>
         </h5>
 
         <h3 className="bold">
           Full stack software developer with 5 years experience building products in the telco and accounting industries
-        </h3>
+        </h3> */}
 
         <ExperienceSection />
         <ProjectsSection />
